a5542efb0a1ef71c22388c11eee201d559e8b6b9
Andrei Vidaicu <AVidaicu@studygroup.com>
2019-09-18 16:04:29 +1000

    initial

---
+++ src/app/form.ts
@@ -0,0 +1,11 @@
+export class Form {
+
+  constructor(
+    public street_number = '',
+    public route = '',
+    public city = '',
+    public state = '',
+    public postcode = '',
+    public country?: '') {}
+
+}
